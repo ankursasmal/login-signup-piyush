6740edbc72d80235b783a0c744303fbc135e7179
ankursasmal <ankursasmal2017@gmail.com>
2026-03-15 13:54:55 +0530

    router name change put to post

--- src/route/router.js
+++ src/route/router.js
@@ -26,7 +26,7 @@ router.get('/auth-user',authGuard,AuthUser);
 
 
 //Task
-router.put('/admin-create-Task/:id',authGuard,adminTask);
+router.post('/admin-create-Task/:id',authGuard,adminTask);
 router.get('/all-Task',authGuard,AllTask);
 router.get('/get-specific-user-Task/:id',authGuard,GetSpecificUserTask);
 router.put('/update-specific-Task/:userId/:index',authGuard,UpdateSpecificTask);
